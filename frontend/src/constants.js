234cf375c63a418e873ad70541cf968b71949aa1
Venkatasaikatepalli <venkatsaisoft@gmail.com>
2018-02-20 22:43:49 +0530

    Frontend: Api url Update for S3 usage

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,11 +1,11 @@
 export const constants = {
-  signup: 'http://localhost/inmar-contacts-management/api/index.php/signup',
-  login: 'http://localhost/inmar-contacts-management/api/index.php/login',
-  get_contacts: 'http://localhost/inmar-contacts-management/api/index.php/contacts',
-  manage_contact: 'http://localhost/inmar-contacts-management/api/index.php/manage_contact',
-  delete_contact: 'http://localhost/inmar-contacts-management/api/index.php/delete_contact',
-  get_contacts_groups: 'http://localhost/inmar-contacts-management/api/index.php/contacts_groups',
-  manage_contacts_group: 'http://localhost/inmar-contacts-management/api/index.php/manage_contacts_group',
-  delete_contacts_group: 'http://localhost/inmar-contacts-management/api/index.php/delete_contacts_group',
-  get_stats: 'http://localhost/inmar-contacts-management/api/index.php/get_stats'
+  signup: 'https://api.codejunkers.xyz/index.php/signup',
+  login: 'https://api.codejunkers.xyz/index.php/login',
+  get_contacts: 'https://api.codejunkers.xyz/index.php/contacts',
+  manage_contact: 'https://api.codejunkers.xyz/index.php/manage_contact',
+  delete_contact: 'https://api.codejunkers.xyz/index.php/delete_contact',
+  get_contacts_groups: 'https://api.codejunkers.xyz/index.php/contacts_groups',
+  manage_contacts_group: 'https://api.codejunkers.xyz/index.php/manage_contacts_group',
+  delete_contacts_group: 'https://api.codejunkers.xyz/index.php/delete_contacts_group',
+  get_stats: 'https://api.codejunkers.xyz/index.php/get_stats'
 }
